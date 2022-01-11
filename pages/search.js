a2d07f475a05721329ee2df2a6e1545264245a17
NRotondaro <niksonrotondaro1@gmail.com>
2022-01-11 08:34:11 -0300

    feat(global): added button style globaly

--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 export default function search() {
   return (
     <div>
       <Header />
+      <main className='flex'>
+        <section>
+          <p className='text-xs'>300+ Stays for 5 number of guests</p>
+          <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in </h1>
+          <div className='hidden lg:inline-flex'>
+            <p className='button'>Cancellation Flexibility</p>
+            <p className='button'>Type of Place</p>
+            <p className='button'>Price</p>
+            <p className='button'>Rooms and Beds</p>
+            <p className='button'>More filters</p>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
